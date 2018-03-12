9b1416d483dc1e39d3bb25156b8bdc5a6b72f169
Shrish Chaturvedi <syrus989@gmail.com>
2018-03-12 17:09:54 -0500

    final game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,37 @@
 $(document).ready(function () {
 
     //// variables
-    var randomNumber = 0;
+    var randomNumber = 0; 
     var winCount = 0;
     var lossCount = 0;
     var userCount = 0;
     var numberOptions = [];
     
+    //// functions
+
+    // function to assign random element from numberOptions[] to button values
     function assignRandomVal(elementID){
         var jewelVal = numberOptions[Math.floor(Math.random()*numberOptions.length)];
         $(elementID).val(jewelVal); 
         numberOptions.splice($.inArray(jewelVal, numberOptions),1);
     }
 
-    //// functions
+    // function to reset game to initial state
     function initGame(){
         userCount = 0;
-        randomNumber = Math.floor(Math.random() * 50) + 30;
-        numberOptions = [10, 1, 3, 7];
+        randomNumber = Math.floor(Math.random() * 101) + 19;
+        // numberOptions = [10, 1, 3, 7];
+
+        // add random values to numberOptions
+        var max = 1;
+        var min = 1;
+        for (i =0; i<4 ; i++){
+        numberOptions.push(Math.floor(Math.random() * (max - min)) + min);
+        min = max+1;
+        max = min+3; 
+        };
 
         // randomly assign values to jewels
-        // $("#ruby").val(10);     
         assignRandomVal("#ruby");   
         assignRandomVal("#diamond");
         assignRandomVal("#amber");
@@ -47,14 +58,12 @@ $(document).ready(function () {
             $("#win").html(winCount);
             $("#statusMsg").html("You Win!");
             setTimeout(initGame, 2000);
-            // initGame();
         }
         else if(userCount > randomNumber){
             lossCount++;
             $("#loss").html(lossCount);
             $("#statusMsg").html("You Lose!");
             setTimeout(initGame, 2000);
-            // initGame();
         }
 
     });   
